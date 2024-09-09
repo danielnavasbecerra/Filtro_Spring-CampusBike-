5d60dbbef990c6a70695593e8cb3231b17f69b8f
danielnavasbecerra <danielnavasb14@gmail.com>
2024-09-09 14:36:25 -0500

    feat: :tada: Start Project

---
+++ src/test/java/com/danavas/campusbike/CampusbikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danavas.campusbike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusbikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
